2b4d8bf2050f39e52c95caa8071339dac4aa2095
chris <chrishughallen@gmail.com>
2020-07-06 12:04:06 +0200

    updated chromedrivermanager

--- test/testChromeDriver.py
+++ test/testChromeDriver.py
@@ -1,11 +1,12 @@
 import PyPDF2
 import re
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 def testChromeDriver():
-    browser = webdriver.Chrome()
     try:
-        browser.get('https://bexar.acttax.com/act_webdev/bexar')
+        driver.get('https://bexar.acttax.com/act_webdev/bexar')
         print("Chromedriver is up to date")
     except:
         print("Chromedriver is not set up correctly")
